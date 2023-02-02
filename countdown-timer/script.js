4be160f31047437a81a855bf4399aae27c936c64
Thu Ya Oo <tyo@mit.com.mm>
2023-02-02 21:45:55 +0630

    training

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,4 +1,4 @@
-const newYear = '1 Jan 2022';
+const newYear = '1 Jan 2024';
 const months = document.getElementById("months")
 const days = document.getElementById("days")
 const hours = document.getElementById("hours")
